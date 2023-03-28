34608bec061c046640e6060a87296a94ffb34391
Bill <55395638+billyan2018@users.noreply.github.com>
2023-03-29 05:59:28 +0800

    refine functions

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const DATA_PREFIX = 'data: ';
+const API_ROOT = 'https://aicursor.com';
+const decoder = new TextDecoder('utf-8');
+
 function readStream(reader, handler) {
   reader.read().then(({ done, value }) => {
     if (done) {
@@ -8,8 +12,8 @@ function readStream(reader, handler) {
     let stopped = false;
     for (let line of lines) {
 
-      if (line.startsWith('data: ')) {
-        const jsonString = line.slice(6)
+      if (line.startsWith(DATA_PREFIX)) {
+        const jsonString = line.slice(DATA_PREFIX.length)
         if (jsonString === '[DONE]') {
           stopped = true;
           break;
@@ -25,12 +29,8 @@ function readStream(reader, handler) {
 
 }
 
-const API_ROOT = 'https://aicursor.com';
-
-const decoder = new TextDecoder('utf-8');
-
-function chat(message, callback) {
-  const data = {
+function buildReqData(message) {
+  return {
     botMessages: [],
     contextType: 'copilot',
     rootPath: '/Desktop/temp',
@@ -46,9 +46,10 @@ function chat(message, callback) {
       currentRootPath: '/temp'
     }
   };
+}
 
-
-  fetch(`${API_ROOT}/conversation`, {
+function buildFetchOption(message) {
+  return {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
@@ -58,20 +59,22 @@ function chat(message, callback) {
       'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Cursor/0.1.7 Chrome/108.0.5359.62 Electron/22.0.0 Safari/537.36',
     },
     'content-type': "application/json",
-    //credentials: 'include',
-    body: JSON.stringify(data),
-  }).then(res => {
+    body: JSON.stringify(buildReqData(message)),
+  };
+}
+
+function chat(message, callback) {
+
+  fetch(`${API_ROOT}/conversation`, buildFetchOption(message)).then(res => {
     const reader = res.body.getReader();
     readStream(reader, callback);
 
   });
 }
 
-// node
-
+// node entry point
 const args = process.argv.slice(2);
 console.log(args);
 chat(args.join(' '), (line) => {
   process.stdout.write(line);
 });
-
