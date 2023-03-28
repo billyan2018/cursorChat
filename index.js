1cabf5f8a431f6a00c6d90dbb3a734b58522b4db
bill <bill.libin.yan@kingland.com>
2023-03-28 16:57:04 +0800

    first commit

---
+++ index.js
@@ -0,0 +1,80 @@
+function readStream(reader, handler) {
+  reader.read().then(({ done, value }) => {
+    if (done) {
+      return;
+    }
+    const rawValue = decoder.decode(value);
+    const lines = rawValue.split('\n');
+    let stopped = false;
+    for (let line of lines) {
+
+      if (line.startsWith('data: ')) {
+        const jsonString = line.slice(6)
+        if (jsonString === '[DONE]') {
+          stopped = true;
+          break;
+        }
+        const content = JSON.parse(jsonString);
+        handler(content);
+      }
+    }
+    if (!stopped) {
+      readStream(reader, handler);
+    }
+  });
+
+}
+
+const API_ROOT = 'https://aicursor.com';
+
+const decoder = new TextDecoder('utf-8');
+
+function addChat(message, callback) {
+  const data = {
+    botMessages: [],
+    contextType: 'copilot',
+    rootPath: '/Desktop/temp',
+    userMessages: [],
+    botMessages: [],
+    userRequest: {
+      precedingCode: [''],
+      suffixCode: [''],
+      copilotCodeBlocks: [],
+      customCodeBlocks: [],
+      codeBlockIdentifiers: [],
+      message: message,
+      msgType: 'freeform',
+      currentRootPath: '/temp'
+    }
+  };
+
+
+  fetch(`${API_ROOT}/conversation`, {
+    method: 'POST',
+    mode: 'cors',
+    headers: {
+      'Content-Type': 'application/json',
+      'accept': '*/*',
+      authority: 'aicursor.com',
+      referer: 'http://localhost:3000/',
+      'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Cursor/0.1.7 Chrome/108.0.5359.62 Electron/22.0.0 Safari/537.36',
+      // Cookie: `repo_path=${state.global.rootPath}`,
+    },
+    'content-type': "application/json",
+    //credentials: 'include',
+    body: JSON.stringify(data),
+  }).then(res => {
+    const reader = res.body.getReader();
+    readStream(reader, callback);
+
+  });
+}
+console.log('loaded');
+// node
+
+const args = process.argv.slice(2);
+console.log(args);
+addChat(args.join(' '), (line) => {
+  process.stdout.write(line);
+});
+
---
+++ readme.md
@@ -0,0 +1,10 @@
+# cursorChat
+A damn simple node based chat app inspired by [cursor](https://github.com/getcursor/cursor)
+
+## Run
+
+```shell
+node index.js "{whatever you want to say}"
+```
+result:
+![](asset/screen.png)
